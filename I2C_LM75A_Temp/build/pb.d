4fdcc4ffb9ba8b9ae941fd860381e911d42cf74b
Syamantak Payra <40369982+spayra1@users.noreply.github.com>
2020-06-12 17:43:45 -0500

    Update I2C scripts

---
+++ I2C_LM75A_Temp/build/pb.d
@@ -0,0 +1,25 @@
+c:/Users/spayr/Documents/eclipseworkspace/MAX32660/I2C_LM75A_Temp/build/pb.o: \
+ c:\maxim\firmware\max32660\libraries\boards\source\pb.c \
+ c:\maxim\toolchain\lib\gcc\arm-none-eabi\6.3.1\include\stddef.h \
+ c:/Maxim/Firmware/MAX32660/Libraries/MAX32660PeriphDriver/Include/mxc_config.h \
+ c:/Maxim/Firmware/MAX32660/Libraries/CMSIS/Device/Maxim/MAX32660/Include/mxc_device.h \
+ c:/Maxim/Firmware/MAX32660/Libraries/CMSIS/Device/Maxim/MAX32660/Include/max32660.h \
+ c:\maxim\toolchain\lib\gcc\arm-none-eabi\6.3.1\include\stdint.h \
+ c:\maxim\toolchain\arm-none-eabi\include\stdint.h \
+ c:\maxim\toolchain\arm-none-eabi\include\machine\_default_types.h \
+ c:\maxim\toolchain\arm-none-eabi\include\sys\features.h \
+ c:\maxim\toolchain\arm-none-eabi\include\_newlib_version.h \
+ c:\maxim\toolchain\arm-none-eabi\include\sys\_intsup.h \
+ c:\maxim\toolchain\arm-none-eabi\include\sys\_stdint.h \
+ c:/Maxim/Firmware/MAX32660/Libraries/CMSIS/Include/core_cm4.h \
+ c:/Maxim/Firmware/MAX32660/Libraries/CMSIS/Include/core_cmInstr.h \
+ c:/Maxim/Firmware/MAX32660/Libraries/CMSIS/Include/core_cmFunc.h \
+ c:/Maxim/Firmware/MAX32660/Libraries/CMSIS/Include/core_cm4_simd.h \
+ c:/Maxim/Firmware/MAX32660/Libraries/CMSIS/Device/Maxim/MAX32660/Include/system_max32660.h \
+ c:/Maxim/Firmware/MAX32660/Libraries/MAX32660PeriphDriver/Include/mxc_errors.h \
+ c:/Maxim/Firmware/MAX32660/Libraries/MAX32660PeriphDriver/Include/mxc_pins.h \
+ c:/Maxim/Firmware/MAX32660/Libraries/MAX32660PeriphDriver/Include/gpio.h \
+ c:/Maxim/Firmware/MAX32660/Libraries/CMSIS/Device/Maxim/MAX32660/Include/gpio_regs.h \
+ c:/Maxim/Firmware/MAX32660/Libraries/MAX32660PeriphDriver/Include/mxc_assert.h \
+ c:\maxim\firmware\max32660\libraries\boards\include\pb.h \
+ c:/Maxim/Firmware/MAX32660/Libraries/MAX32660PeriphDriver/Include/gpio.h
